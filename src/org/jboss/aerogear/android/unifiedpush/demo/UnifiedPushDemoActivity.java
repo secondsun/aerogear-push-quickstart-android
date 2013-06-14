679f06e3377ba27e253fa816afc9551e1cdc4d74
Daniel Passos <daniel@passos.me>
2013-06-14 13:38:17 -0300

    Create app skeleton

---
+++ src/org/jboss/aerogear/android/unifiedpush/demo/UnifiedPushDemoActivity.java
@@ -0,0 +1,15 @@
+package org.jboss.aerogear.android.unifiedpush.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UnifiedPushDemoActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
